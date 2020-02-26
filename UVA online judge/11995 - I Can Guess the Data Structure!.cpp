1412a16554f822c1bc0f847a6d053701c2ab8c96
mc-cari <42097398+mc-cari@users.noreply.github.com>
2020-02-25 21:18:06 -0300

    Update 11995 - I Can Guess the Data Structure!.cpp

--- UVA online judge/11995 - I Can Guess the Data Structure!.cpp
+++ UVA online judge/11995 - I Can Guess the Data Structure!.cpp
@@ -7,7 +7,6 @@ typedef pair<ll,ll> pll;
 double pi = 2 * acos(0.0);
 #define ALL(x) x.begin() , x.end()
 #define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
-//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3146
 int main()
 {
     int n;
@@ -18,10 +17,11 @@ int main()
         queue<int>q;
         priority_queue<int>p;
         int x,y;
-        bool o1=1,o2=1,o3=1;
+        bool o1=1,o2=1,o3=1,o=0;
         while(n--)
         {
             cin>>x>>y;
+            if(o)continue;
             if(x==1)
             {
                 s.push(y);
@@ -30,6 +30,14 @@ int main()
             }
             else
             {
+                if(s.empty())
+                {
+                    o=1;
+                    o1=0;
+                    o2=0;
+                    o3=0;
+                    continue;
+                }
                 if(s.top()!=y)
                 {
                     o1=0;
@@ -51,7 +59,7 @@ int main()
         else if(o2)
             cout<<"queue\n";
         else
-            cout<<"priority_queue\n";
+            cout<<"priority queue\n";
 
     }
 
