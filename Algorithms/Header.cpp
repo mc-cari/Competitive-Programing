02d0feeb49101b0dd6e916709b863955364dc15e
Mauricio Cari Leal <mauricio.cari@uc.cl>
2022-02-03 18:47:57 -0300

    feat(algorithms): add header

---
+++ Algorithms/Header.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vp;
+typedef double db;
+#define INF 1e17
+#define EPS 1e-7
+#define ALL(x) x.begin() , x.end()
+#define ALLR(x) x.rbegin() , x.rend()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define PI acos(-1.0)
+
+int main() {
+
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Code here
+    
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
