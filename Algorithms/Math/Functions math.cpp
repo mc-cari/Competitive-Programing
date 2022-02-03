4125e1e0d4cd2fa7b2ccf827b587fe4700874043
Mauricio Cari Leal <mauricio.cari@uc.cl>
2022-02-03 18:45:27 -0300

    refactor(algorithms): Add folders

--- Algorithms/Math/Functions math.cpp
+++ Algorithms/Math/Functions math.cpp
@@ -241,6 +241,45 @@ ll Fibo(ll n) {
     return V[0][0];
 }
 
+// C function for extended Euclidean Algorithm
+int gcdExtended(int a, int b, int *x, int *y);
+
+// Function to find modulo inverse of a
+void modInverse(int a, int m)
+{
+    int x, y;
+    int g = gcdExtended(a, m, &x, &y);
+    if (g != 1)
+        cout << "Inverse doesn't exist";
+    else
+    {
+        // m is added to handle negative x
+        int res = (x%m + m) % m;
+        cout << "Modular multiplicative inverse is " << res;
+    }
+}
+
+// C function for extended Euclidean Algorithm
+int gcdExtended(int a, int b, int *x, int *y)
+{
+    // Base Case
+    if (a == 0)
+    {
+        *x = 0, *y = 1;
+        return b;
+    }
+
+    int x1, y1; // To store results of recursive call
+    int gcd = gcdExtended(b%a, a, &x1, &y1);
+
+    // Update x and y using results of recursive
+    // call
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+
+    return gcd;
+}
+
 //https://github.com/PabloMessina/Competitive-Programming-Material/blob/master/Mathematics/chinese_remainder_theorem.cpp
 
 inline ll mod(ll x, ll m) { return ((x %= m) < 0) ? x+m : x; }
